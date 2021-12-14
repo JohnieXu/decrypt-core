570b6737fa91813601b6e05f394b53919004336c
JohnieXu <281910378@qq.com>
2021-12-14 23:05:35 +0800

    feat: 添加 typescript 类型声明文件

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,17 @@
 // https://segmentfault.com/a/1190000009247663
 
 declare namespace decryptCore {
+  /**
+   * 加密
+   * @param key 秘钥
+   * @param data 待加密数据
+   */
   function decrypt(key: string, data: string): string
+  /**
+   * 解密
+   * @param key 秘钥
+   * @param data 待解密数据
+   */
   function encrypt(key: string, data: string): string
 }
 
--- types/util.d.ts
+++ types/util.d.ts
@@ -1,28 +1,67 @@
-declare function s2as(length: number): string
-declare function strToHexCharCode(str: string): string
-declare function hexCharCodeToStr(str: string): string
-declare function padding80(data: string, toHex: boolean): string
-declare function byteToString(arr: number[]): string
-declare function stringToByte(str: string): number[]
-
 declare module "decrypt-core/es/util" {
-  export {
-    s2as,
-    strToHexCharCode,
-    hexCharCodeToStr,
-    padding80,
-    byteToString,
-    stringToByte
-  }
+  /**
+   * 生成随机字符串
+   * @param length 长度
+   */
+  export function s2as(length: number): string
+  /**
+   * 字符串转16进制
+   * @param str 字符串
+   */
+  export function strToHexCharCode(str: string): string
+  /**
+   * 16进制字符日转普通字符串
+   * @param str 16 进制字符串
+   */
+  export function hexCharCodeToStr(str: string): string
+  /**
+   * 补80
+   * @param data 数据
+   * @param toHex 是否转换为16进制
+   */
+  export function padding80(data: string, toHex: boolean): string
+  /**
+   * 字节数组转字符串
+   * @param arr 字节数组
+   */
+  export function byteToString(arr: number[]): string
+  /**
+   * 字符串转字节数组
+   * @param str 字符串
+   */
+  export function stringToByte(str: string): number[]
 }
 
 declare module "decrypt-core/lib/util" {
-  export {
-    s2as,
-    strToHexCharCode,
-    hexCharCodeToStr,
-    padding80,
-    byteToString,
-    stringToByte
-  }
+  /**
+   * 生成随机字符串
+   * @param length 长度
+   */
+   export function s2as(length: number): string
+   /**
+    * 字符串转16进制
+    * @param str 字符串
+    */
+   export function strToHexCharCode(str: string): string
+   /**
+    * 16进制字符日转普通字符串
+    * @param str 16 进制字符串
+    */
+   export function hexCharCodeToStr(str: string): string
+   /**
+    * 补80
+    * @param data 数据
+    * @param toHex 是否转换为16进制
+    */
+   export function padding80(data: string, toHex: boolean): string
+   /**
+    * 字节数组转字符串
+    * @param arr 字节数组
+    */
+   export function byteToString(arr: number[]): string
+   /**
+    * 字符串转字节数组
+    * @param str 字符串
+    */
+   export function stringToByte(str: string): number[]
 }
